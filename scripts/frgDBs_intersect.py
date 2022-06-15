830d20398ee2c85e918623007e99711c08ce928b
IFilella <isaacfilella@gmail.com>
2022-06-15 14:48:51 +0200

    New molDB class added in mol.py library as well as a function to intersect molDB

---
+++ scripts/frgDBs_intersect.py
@@ -0,0 +1,23 @@
+import mol
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description ='Given two fragment databases with the format (SMILE simSMILES IDs), intersect them')
+    parser.add_argument('-db1', dest="db1", help = "First database of fragments as txt file")
+    parser.add_argument('-db2', dest="db2", help = "Second database of fragments as txt file")
+    parser.add_argument('-t', dest="simt", default = 1, help = "Similariy threshold")
+    parser.add_argument('-o', dest="out", default = None, help = "To output the intersected molDB")
+    args = parser.parse_args()
+    fdb1 = args.db1
+    fdb2 = args.db2
+    simt = float(args.simt)
+    out = args.out
+
+    #db1 = mol.molDB(txtDB=fdb1,paramaters=True,verbose=True)
+    #db1.save_molDB(fdb1.replace('.txt',''))
+    #db2 = mol.molDB(txtDB=fdb2,paramaters=True,verbose=True) 
+    #db2.save_molDB(fdb2.replace('.txt',''))
+    db1 = mol.molDB(dicDB=fdb1.replace('.txt','.p'),paramaters=True,verbose=True)
+    db2 = mol.molDB(dicDB=fdb2.replace('.txt','.p'),paramaters=True,verbose=True)
+    
+    db3 = mol.intersect_molDBs(db1,db2,simt,out)
--- scripts/mol.py
+++ scripts/mol.py
@@ -5,6 +5,57 @@ from rdkit import DataStructs
 import re
 import warnings
 import rdkit.Chem.Lipinski as Lipinski
+import pickle
+import copy
+
+def intersect_molDBs(db1,db2,simt,output=None,verbose=True):
+    db3 = copy.deepcopy(db1)
+    keepkeys = []
+    hitsSMILE = 0
+    hitsSimilarity = 0
+    for i,k1 in enumerate(db1.dicDB.keys()):
+        m1 = db1.dicDB[k1][2]
+        SMILE1 = k1
+        for k2 in db2.dicDB.keys():
+            m2 = db2.dicDB[k2][2]
+            SMILE2 = k2
+            if SMILE1 == SMILE2:
+                hitsSMILE += 1
+                if verbose: print(i,'bySMILE',SMILE1,SMILE2)
+                keepkeys.append(SMILE1)
+                break
+            if m1.NumAtoms != m2.NumAtoms: continue
+            if m1.NOCount != m2.NOCount: continue
+            if m1.NHOHCount != m2.NHOHCount: continue
+            if m1.rings != m2.rings: continue
+            if m1.sp3 != m2.sp3: continue
+            if m1.NumAliphaticRings != m2.NumAliphaticRings: continue
+            if m1.NumAromaticRings != m2.NumAromaticRings: continue
+            similarity = get_MolSimilarity(m1,m2)
+            if similarity >= simt:
+                hitsSimilarity += 1
+                if verbose: print(i,'bySimilarity',SMILE1,SMILE2)
+                keepkeys.append(SMILE1)
+                break
+        if i == 500: break
+    totalhits = hitsSMILE + hitsSimilarity
+    sizedb1 = len(db1.dicDB.keys())
+    sizedb2 = len(db2.dicDB.keys())
+    print(keepkeys)
+    print('Hits by SMILE: %d'%hitsSMILE)
+    print('Hits by Similarity (threshold %.3f): %d'%(simt,hitsSimilarity))
+    print('Total hits: %d'%totalhits)
+    print('Total db1 = %d, total db2 = %d'%(sizedb1,sizedb2))
+    print('Percentage of elements of db1 in db2: %f'%(((float(totalhits)/float(sizedb1))*100)))
+    print('Percentage of elements of db2 in db1: %f'%(((float(totalhits)/float(sizedb2))*100)))
+    if output != None:
+        set_keepkeys = set(keepkeys)
+        set_db3keys = set(list(db3.dicDB.keys()))
+        delkeys = list(set_db3keys.difference(set_keepkeys))
+        for key in delkeys:
+            del db3.dicDB[key]
+        print(len(db3.dicDB.keys()))
+        db3.save_molDB(output+'p')
 
 def filter_db_similarity(indb,outdb,verbose=True):
     inp = open(indb,'r')
@@ -18,33 +69,33 @@ def filter_db_similarity(indb,outdb,verbose=True):
         IDs = line[-1]
         m1 = mol(smile=SMILE)
         m1_atoms = m1.mol.GetNumAtoms()
-        m1_NOcount = Lipinski.NOCount(m1.mol)
-        m1_NHOHcount = Lipinski.NHOHCount(m1.mol)
+        m1_NOCount = Lipinski.NOCount(m1.mol)
+        m1_NHOHCount = Lipinski.NHOHCount(m1.mol)
         m1_rings = Lipinski.RingCount(m1.mol)
         m1_sp3 = Lipinski.FractionCSP3(m1.mol)
         m1_NumAliphaticRings = Lipinski.NumAliphaticRings(m1.mol)
         m1_NumAromaticRings = Lipinski.NumAromaticRings(m1.mol)
         if len(uniquefrags.keys()) == 0:
-            uniquefrags[SMILE] = [SMILE,IDs,m1,m1_atoms,m1_NOcount,m1_NHOHcount,m1_rings,m1_sp3,m1_NumAliphaticRings,m1_NumAromaticRings]
+            uniquefrags[SMILE] = [SMILE,IDs,m1,m1_atoms,m1_NOCount,m1_NHOHCount,m1_rings,m1_sp3,m1_NumAliphaticRings,m1_NumAromaticRings]
         for j,k in enumerate(uniquefrags.keys()):
             m2 = uniquefrags[k][2]
             m2_atoms = uniquefrags[k][3]
-            m2_NOcount = uniquefrags[k][4]
-            m2_NHOHcount = uniquefrags[k][5]
+            m2_NOCount = uniquefrags[k][4]
+            m2_NHOHCount = uniquefrags[k][5]
             m2_rings = uniquefrags[k][6]
             m2_sp3 = uniquefrags[k][7]
             m2_NumAliphaticRings = uniquefrags[k][8]
             m2_NumAromaticRings = uniquefrags[k][9]
-            if m1_atoms != m2_atoms or m1_NOcount != m2_NOcount or m1_rings != m2_rings or m1_NHOHcount != m2_NHOHcount or m1_sp3 != m2_sp3 or m1_NumAliphaticRings != m2_NumAliphaticRings or  m1_NumAromaticRings != m2_NumAromaticRings:
+            if m1_atoms != m2_atoms or m1_NOCount != m2_NOCount or m1_rings != m2_rings or m1_NHOHCount != m2_NHOHCount or m1_sp3 != m2_sp3 or m1_NumAliphaticRings != m2_NumAliphaticRings or  m1_NumAromaticRings != m2_NumAromaticRings:
                 if j == len(uniquefrags.keys())-1:
-                    uniquefrags[SMILE] = [SMILE,IDs,m1,m1_atoms,m1_NOcount,m1_NHOHcount,m1_rings,m1_sp3,m1_NumAliphaticRings,m1_NumAromaticRings]
+                    uniquefrags[SMILE] = [SMILE,IDs,m1,m1_atoms,m1_NOCount,m1_NHOHCount,m1_rings,m1_sp3,m1_NumAliphaticRings,m1_NumAromaticRings]
                     break
                 else:
                     continue
             else:
                 similarity =  get_MolSimilarity(m1,m2)
                 if j == len(uniquefrags.keys())-1 and similarity != 1:
-                    uniquefrags[SMILE] = [SMILE,IDs,m1,m1_atoms,m1_NOcount,m1_NHOHcount,m1_rings,m1_sp3,m1_NumAliphaticRings,m1_NumAromaticRings]
+                    uniquefrags[SMILE] = [SMILE,IDs,m1,m1_atoms,m1_NOCount,m1_NHOHCount,m1_rings,m1_sp3,m1_NumAliphaticRings,m1_NumAromaticRings]
                     break
                 elif j == len(uniquefrags.keys())-1 and similarity == 1:
                     print("0: " + uniquefrags[k][0] + " " +SMILE)
@@ -100,35 +151,50 @@ def get_no_anchoring_frag(frag):
     noanfrag = re.sub("\(\)","",noanfrag)
     return noanfrag
 
-class fragDB(object):
+class molDB(object):
     """""
+    Object containing a database of molecules/fragments.
+    It reads a file 'txtDB' of format (SMILE equivalentSMILES IDs) and transform it into a dicc 'dicDB'
+    which can also contain physical paramaters of the molecules/fragments if paramaters flag is given.
+    It can also load a precalculated 'dicDB' molDB object.
     """""
-    def __init__(self, txtDB = None, dicDB = None):
+    def __init__(self, txtDB = None, dicDB = None, paramaters = False, verbose = False):
         self.txtDB = txtDB
         self.dicDB = dicDB
         if self.txtDB != None and self.dicDB == None:
             self.dicDB = {}
             db = open(self.txtDB,'r')
-            for line in db:
+            for i,line in enumerate(db):
                 line = line.split()
                 SMILE = line[0]
-                simSMILES = line[1]
+                eqSMILES = line[1]
                 IDs = line[2]
-                m1 = mol(smile=SMILE)
-                m1_atoms = m1.mol.GetNumAtoms()
-                m1_NOcount = Lipinski.NOCount(m1.mol)
-                m1_NHOHcount = Lipinski.NHOHCount(m1.mol)
-                m1_rings = Lipinski.RingCount(m1.mol)
-                m1_sp3 = Lipinski.FractionCSP3(m1.mol)
-                m1_NumAliphaticRings = Lipinski.NumAliphaticRings(m1.mol)
-                m1_NumAromaticRings = Lipinski.NumAromaticRings(m1.mol)
-                self.dicDB[SMILE] = [simSMILES,]
+                m1 = mol(smile=SMILE,allparamaters = paramaters)
+                if verbose: print(i+1,SMILE)
+                self.dicDB[SMILE] = [eqSMILES,IDs,m1]
+        elif self.txtDB == None and self.dicDB != None:
+            with open(self.dicDB, 'rb') as handle:
+                self.dicDB = pickle.load(handle)
+        elif self.txtDB == None and self.dicDB == None:
+            raise KeyError('Either a txtDB or a dicDB must be provided')
+        else:
+            raise KeyError('Provide only a txtDB or a dicDB, not both simultaniusly')
             
+    def save_molDB(self,output):
+        with open(output+'.p', 'wb') as handle:
+            pickle.dump(self.dicDB, handle)
+
+    def print_molDB(self,output):
+        f = open(output+'.txt','w')
+        for k in self.dicDB.keys():
+            f.write(k + " " + self.dicDB[k][0] + " " + self.dicDB[k][1] + '\n')
+        f.close()
+
 
 class mol(object):
     """"""
     """"""
-    def __init__(self ,smile = None, InChI = None):
+    def __init__(self ,smile = None, InChI = None, allparamaters = False):
         if smile != None and InChI == None:
             self.smile = smile
             self.mol = Chem.MolFromSmiles(self.smile)
@@ -142,7 +208,8 @@ class mol(object):
             self.mol = Chem.MolFromInchi(self.InChI)
         else:
             raise ValueError('To initialize the molecule SMILE or InChI format is required')
-
+        if allparamaters:
+            self.get_AllParamaters()
 
     def get_BRICSdecomposition(self):
         self.fragments = list(BRICS.BRICSDecompose(self.mol))
@@ -150,28 +217,44 @@ class mol(object):
     def get_clean_fragments(self):
         if self.fragments == None: self.get_BRICSdecomposition()
         self.cfragments = [re.sub("(\[.*?\])", "[*]", frag) for frag in self.fragments]
-    
+   
+    def get_AllParamaters(self):
+        self.get_NumAtoms()
+        self.get_NOCount()
+        self.get_NHOHCount()
+        self.get_rings()
+        self.get_sp3()
+        self.get_NumAliphaticRings()
+        self.get_NumAromaticRings()
+
     def get_NumAtoms(self):
         self.NumAtoms = self.mol.GetNumAtoms()
+        return self.NumAtoms
 
     def get_NOCount(self):
         self.NOCount = Lipinski.NOCount(self.mol)
+        return self.NOCount
 
-    def get_NHOHcount(self):
-        self.NHOHcount = Lipinski.NHOHCount(self.mol)
+    def get_NHOHCount(self):
+        self.NHOHCount = Lipinski.NHOHCount(self.mol)
+        return self.NHOHCount
 
     def get_rings(self):
         self.rings = Lipinski.RingCount(self.mol)
+        return self.rings
 
     def get_sp3(self):
         self.sp3 = Lipinski.FractionCSP3(self.mol)
+        return self.sp3
 
     def get_NumAliphaticRings(self):
         self.NumAliphaticRings = Lipinski.NumAliphaticRings(self.mol)
+        return self.NumAliphaticRings
 
     def get_NumAromaticRings(self):
         self.NumAromaticRings = Lipinski.NumAromaticRings(self.mol)
-        
+        return self.NumAromaticRings
+
 if __name__ == '__main__':
     smile = 'Cc1cc(-c2csc(N=C(N)N)n2)cn1C'
     inchi = 'InChI=1S/C10H13N5S/c1-6-3-7(4-15(6)2)8-5-16-10(13-8)14-9(11)12/h3-5H,1-2H3,(H4,11,12,13,14)'
