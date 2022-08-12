70314e8d9657d3a7dd426c0e3e7cb9a0bc3631cb
IFilella <isaacfilella@gmail.com>
2022-08-12 15:26:10 +0200

    new versions of PDB_download.py and PDB_extract.py

--- scripts/PDB_download.py
+++ scripts/PDB_download.py
@@ -1,20 +1,34 @@
 from prody import *
 import numpy as np
 import argparse
+import os
 
 if __name__ == '__main__':
-     parser = argparse.ArgumentParser(description ='Given a PDB list from https://www.rcsb.org/ download all associated .pdb.gz files')
-     parser.add_argument('-l', dest="pdblist", help = "List of PDBs",required=True)
-     parser.add_argument('-o', dest="outdir", help = "Output directory to store the .pdb.gz files",required=True)
-     args = parser.parse_args()
+    parser = argparse.ArgumentParser(description ='Given a PDB list from https://www.rcsb.org/ download all associated .pdb.gz files')
+    parser.add_argument('-l', dest="pdblist", help = "List of PDBs",required=True)
+    parser.add_argument('-o', dest="outdir", help = "Output directory to store the PDBs",default=None)
+    args = parser.parse_args()
 
-     pdblist = args.pdblist
-     outdir = args.outdir
-
+    #Parse inputs
+    pdblist = args.pdblist
     PDB_IDs = np.genfromtxt(pdblist,dtype=str,delimiter=',')
+    outdir = args.outdir
+    if outdir != None:
+        try:
+            os.chdir(outdir)
+        except:
+            os.makedirs(outdir)
+            os.chdir(outdir)
+    else:
+        if os.path.isdir('dow_PDBs/'):
+            os.chdir('dow_PDBs/')
+        else:
+            os.makedirs('dow_PDBs/')
+            os.chdir('dow_PDBs/')
+
     print('There are %d PDBs to download'%len(PDB_IDs))
 
     #Download the PDBs
     for PDB_ID in PDB_IDs:
         print(PDB_ID)
-        pdb = parsePDB('%s/%s.pdb.gz'%(outdir,PDB_ID))
+        pdb = parsePDB('%s'%(PDB_ID))
