d028ee3f914f0be8e7a61991615275529960e1e1
juliavilmor <juliavilmor@gmail.com>
2023-03-21 12:11:52 +0100

    linewidth scatterplot

--- plotlib.py
+++ plotlib.py
@@ -26,12 +26,12 @@ def plot_UMAP(dbs,names,output=None, random_max = None, delimiter = None, fpsalg
     print('Shape of UMAP_results: ', UMAP_results.shape)
     _plot_reducer(reducer_results = UMAP_results, Y=Y, output=output, colors=colors, sizes=S, alphas=A)
 
-def plot_tSNE(dbs,names,output=None, random_max = None, delimiter = None, fpsalg = 'RDKIT',colors = None, sizes=None, alphas = None, n_iter=1000, perplexity=30, early_exaggeration=12,learning_rate='auto'):
+def plot_tSNE(dbs,names,output=None, random_max = None, delimiter = None, fpsalg = 'RDKIT',colors = None, sizes=None, alphas = None, linewidths=None, n_iter=1000, perplexity=30, early_exaggeration=12,learning_rate='auto'):
     X, Y, S, A = _prepare_reducer(dbs,names,random_max, delimiter, fpsalg, sizes, alphas)
     print('Computing TSNE')
     tsne = TSNE(n_components=2, verbose = 1, learning_rate=learning_rate, init='pca', perplexity=perplexity, n_iter=n_iter, metric='hamming',early_exaggeration=early_exaggeration)
     tsne_results = tsne.fit_transform(X)
-    _plot_reducer(reducer_results = tsne_results, Y=Y, output=output, colors=colors, sizes=S, alphas=A)
+    _plot_reducer(reducer_results = tsne_results, Y=Y, output=output, colors=colors, sizes=S, alphas=A, linewidths=linewidths)
 
 def plot_PCA(dbs,names,output=None, random_max = None, delimiter = None, fpsalg = 'RDKIT', colors = None, sizes=None, alphas = None):
     X, Y, S, A = _prepare_reducer(dbs,names,random_max, delimiter, fpsalg, sizes, alphas)
@@ -47,6 +47,7 @@ def _prepare_reducer(dbs,names,random_max, delimiter, fpsalg, sizes,alphas):
     Y = []
     S = []
     A = []
+
     for i,db in enumerate(dbs):
         if delimiter == None:
             name = names[i]
@@ -68,16 +69,18 @@ def _prepare_reducer(dbs,names,random_max, delimiter, fpsalg, sizes,alphas):
     X = np.asarray(X)
     return X, Y, S, A
 
-def _plot_reducer(reducer_results,Y,output,colors,sizes,alphas):
+def _plot_reducer(reducer_results,Y,output,colors,sizes,alphas, linewidths):
     df = pd.DataFrame(dict(xaxis=reducer_results[:,0], yaxis=reducer_results[:,1],  molDB = Y, sizes=sizes, alphas=alphas))
-    plt.figure()
+    plt.figure(figsize=(8, 8))
     if colors == None:
         g = sns.scatterplot(data=df, x='xaxis', y='yaxis', hue='molDB', alpha=alphas, size='sizes')
     else:
         g = sns.scatterplot(data=df, x='xaxis', y='yaxis', hue='molDB',palette=colors, alpha=alphas, size='sizes')
+    if linewidths:
+        g = sns.scatterplot(data=df, x='xaxis', y='yaxis', hue='molDB', alpha=alphas, palette=colors, size='sizes', linewidth=linewidths)
     h,l = g.get_legend_handles_labels()
     n = len(set(df['molDB'].values.tolist()))
     plt.legend(h[0:n+1],l[0:n+1])#,bbox_to_anchor=(1.05, 1)), loc=2, borderaxespad=0.)
     plt.tight_layout()
     if output != None:
-        plt.savefig(output+'.pdf',dpi=300)
+        plt.savefig(output+'.pdf',dpi=500)
